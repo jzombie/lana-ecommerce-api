cc99820b36d8de99fb9d47c3fc6a92f94e4f481a
Jeremy Harris <jeremy.harris@zenosmosis.com>
2019-12-29 16:06:05 -0500

    Fixed incorrect Express types

--- src/initExpressApp.ts
+++ src/initExpressApp.ts
@@ -5,8 +5,10 @@ const app = express();
 
 /**
  * Initializes Express application.
+ *
+ * @return {Promise<express.Application>}
  */
-const initExpressApp = async (): Promise<any /*Express.Application*/> => {
+const initExpressApp = async (): Promise<express.Application> => {
   await initSequelize(true);
 
   app.use("/", routes);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,18 @@
 import express from "express";
-const router = express.Router();
 import Product from "../classes/Product";
 import logger from "../logger";
+const router = express.Router();
 
-router.get("/product/:sku", async (req: Express.Request, res: Express.Response) => {
+router.get("/product/:sku", async (req: express.Request, res: express.Response) => {
   handleRoute(req, res, async () => {
-    const sku = (req as any).params.sku; // res.params does not exist on Express.Response (?)
+    const sku = req.params.sku;
     const product = new Product(sku);
 
     return product.fetchDetail();
   });
 });
 
-router.get("/products", async (req: Express.Request, res: Express.Response) => {
+router.get("/products", async (req: express.Request, res: express.Response) => {
   handleRoute(req, res, async () => {
     const products = await Product.fetchAllProducts();
 
@@ -25,19 +25,22 @@ router.get("/products", async (req: Express.Request, res: Express.Response) => {
 /**
  * Unified handling for API routes.
  *
- * @param {Express.Request} req
- * @param {Express.Response} res
- * @param {function: Promise<object>} routeHandler
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {function: Promise<any | Error>} routeHandler The resolved return of which is
+ * sent through the res (express.Response) object back to the client.
  */
 const handleRoute = async (
-  req: Express.Request,
-  res: Express.Response,
-  routeHandler: () => Promise<object>): Promise<void> => {
+  req: express.Request,
+  res: express.Response,
+  routeHandler: () => Promise<any | Error>): Promise<void> => {
   try {
+    logger.info(`HTTP Request | ${req.method} | ${req.url}`);
+
     const result = await routeHandler();
 
     if (typeof result === "object") {
-      (res as any).json(result); // res.json does not exist on Express.Response (?)
+      res.json(result);
     } else {
       throw new Error(`Unhandled response type: ${typeof result}`);
     }
@@ -47,7 +50,7 @@ const handleRoute = async (
 
     logger.error(errMsg);
 
-    (res as any) // res.status does not exist on Express.Response (?)
+    (res as any)
       .status(404)
       .json({
         error: errMsg,
